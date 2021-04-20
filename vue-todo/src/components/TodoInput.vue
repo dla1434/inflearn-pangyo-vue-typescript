11c61973c1db5282b35bca3a412ae6cf4ba1e76e
lim jong hyun <dla1434@naver.com>
2021-04-21 01:55:03 +0900

    https://www.inflearn.com/course/vue-ts/lecture/68020?tab=curriculum 완료

---
+++ vue-todo/src/components/TodoInput.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <label for="todo-input">오늘 할 일 : </label>
+    <input id="todo-input" type="text" :value="item" @input="handleInput" />
+    <button @click="addTodo" type="button">ADD</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  props: ["item"],
+  methods: {
+    handleInput(event: any) {
+      this.$emit("input", event.target.value);
+    },
+    addTodo() {
+      console.log("add");
+      this.$emit("add");
+    },
+  },
+});
+</script>
+
+<style scoped>
+</style>
