24e81ffe7771ecd966cdfaa429078e01cbe3541f
lim jong hyun <dla1434@naver.com>
2021-04-23 00:23:17 +0900

    project2 start 개발 환경 구성

---
+++ vue-news/src/store/actions.js
@@ -0,0 +1,46 @@
+import {fetchAskList, fetchItemInfo, fetchJobsList, fetchList, fetchNewsList, fetchUserInfo} from "../api";
+
+export default {
+    FETCH_NEWS({commit}) {
+        return fetchNewsList()
+            .then(response => {
+                commit('SET_NEWS', response.data);
+                return response;
+            })
+            .catch(error => console.log(error));
+    },
+    FETCH_JOBS({commit}) {
+        return fetchJobsList()
+            .then(({data}) => {
+                commit('SET_JOBS', data);
+            })
+            .catch(error => console.log(error));
+    },
+    FETCH_ASK({commit}){
+        return fetchAskList()
+            .then(({data}) => {
+                commit('SET_ASK', data);
+            })
+            .catch(error => console.log(error));
+    },
+    FETCH_USER({commit}, name){
+        return fetchUserInfo(name)
+            .then(({data}) => {
+                commit('SET_USER', data);
+            })
+            .catch(error => console.log(error));
+    },
+    FETCH_ITEM({commit}, itemId){
+        return fetchItemInfo(itemId)
+            .then(({data}) => {
+                commit('SET_ITEM',data);
+            })
+            .catch(error => console.log(error));
+    },
+    FETCH_LIST({commit}, pageName){
+        return fetchList(pageName)
+            .then(({data}) => commit('SET_LIST',data))
+            .catch(error => console.log(error));
+    }
+
+}
