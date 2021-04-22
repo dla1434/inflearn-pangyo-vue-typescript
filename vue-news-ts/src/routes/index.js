f50a3cd3b0bd5f16d4695669351025f153ed22df
lim jong hyun <dla1434@naver.com>
2021-04-23 01:36:47 +0900

    1차 실행본 완료 - 기본 실행 및 스타일 안 깨지만 에러는 아직 많은 상태

---
+++ vue-news-ts/src/routes/index.js
@@ -0,0 +1,38 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import ItemView from "../views/ItemView";
+import UserView from "../views/UserView";
+import createListView from "../views/CreateListView";
+
+Vue.use(VueRouter);
+
+export const router = new VueRouter({
+  mode: "history",
+  routes: [
+    {
+      // path: url 주소
+      path: "/news",
+      name: "news",
+      // component: url 주소로 갔을 때 표시될 컴포넌트
+      component: createListView("NewsView"),
+    },
+    {
+      path: "/ask",
+      name: "ask",
+      component: createListView("AskView"),
+    },
+    {
+      path: "/jobs",
+      name: "jobs",
+      component: createListView("JobsView"),
+    },
+    {
+      path: "/item/:id",
+      component: ItemView,
+    },
+    {
+      path: "/user/:id",
+      component: UserView,
+    },
+  ],
+});
---
+++ vue-news-ts/src/store/index.js
@@ -0,0 +1,27 @@
+import Vue from "vue";
+import Vuex from "vuex";
+import mutations from "./mutations";
+import actions from "./actions";
+
+Vue.use(Vuex);
+
+export const store = new Vuex.Store({
+  state: {
+    news: [],
+    jobs: [],
+    ask: [],
+    user: {},
+    item: {},
+    list: [],
+  },
+  getters: {
+    fetchedAsk(state) {
+      return state.ask;
+    },
+    fetchedItem(state) {
+      return state.item;
+    },
+  },
+  mutations: mutations,
+  actions: actions,
+});
